dd365491c58aab40872aa381c9d43494bba321c5
AndreyDvorskiy <82935291+AndreyDvorskiy@users.noreply.github.com>
2021-05-03 15:42:47 +0300

    Домашняя работа

---
+++ js/exemple.js
@@ -0,0 +1,28 @@
+var Andrey = {
+    name: 'Andrey',
+    email: 'Andrey@gmail.com'
+}
+var Aleksey = {
+    name: 'Aleksey',
+    email: 'Aleksey@gmail.com'
+}
+var Denis = {
+    name: 'Denis',
+    email: 'Denis@gmail.com'
+}
+var Igor = {
+    name: 'Igor',
+    email: 'Igor@gmail.com'
+}
+var Bogdan = {
+    name: 'Bogdan',
+    email: 'Bogdan@gmail.com'
+}
+var Sergey = {
+    name: 'Sergey',
+    email: 'Sergey@gmail.com'
+}
+var new_obj = [Andrey, Aleksey, Denis, Igor, Bogdan, Sergey];
+var Password = prompt ('Введите номер:');
+console.log(new_obj[Password]);
+
